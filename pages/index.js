f3ad65779767a6e8e91aa86d8d30dc16c9bcd4f8
Andy Hong <andyhongoc@gmail.com>
2020-05-25 13:19:41 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,146 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { Mail, GitHub, Linkedin, Instagram } from "react-feather";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Andy Hong</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="header">
+          <h1>Hi, I'm Andy.</h1>
+          <p>
+            A self-taught developer passionate about helping teams operate
+            effectively to successfully reach their goals.
+          </p>
+          <div className="links">
+            <a href="mailto:andyhongoc@gmail.com">
+              <Mail color="#111111" size={24} />
+            </a>
+            <a href="https://github.com/andyhong">
+              <GitHub color="#111111" size={24} />
+            </a>
+            <a href="https://www.linkedin.com/in/andy-hong-067385118">
+              <Linkedin color="#111111" size={24} />
+            </a>
+            <a href="https://www.instagram.com/andyhxng">
+              <Instagram color="#111111" size={24} />
+            </a>
+          </div>
+        </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <hr />
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="projects">
+          <h2>Projects</h2>
+          <h3>
+            <a href="https://churchkey.io">Churchkey</a>
+          </h3>
+          <p>
+            An online bulletin solution for churches to create a simple and
+            frictionless hand-off experience.
+          </p>
+          <h3>Glimpse</h3>
+          <p>
+            <strong>Coming soon</strong> &mdash; a custom built GUI for PCO that
+            displays all relevant information and milestones for a single person
+            in real time.
+          </p>
+        </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <hr />
 
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="blog">
+          <h2>Blog</h2>
+          <h3>Coming soon!</h3>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .header {
+          max-width: 450px;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .links {
+          margin-top: 1.5rem;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .links a {
+          margin-right: 0.75rem;
         }
 
-        footer img {
-          margin-left: 0.5rem;
+        h1 {
+          font-size: 3rem;
+          margin: 0.25rem 0;
         }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        h2 {
+          font-size: 2.5rem;
+          margin-bottom: 2rem;
         }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
+        h3 {
           font-size: 1.5rem;
+          margin-bottom: 0.25rem;
+          color: #0074d9;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        h3 a:visited {
+          color: #0074d9;
         }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
+        p {
+          font-size: 1rem;
+          margin-bottom: 2rem;
+          line-height: 1.2;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+        hr {
+          width: 100px;
+          margin: 2rem 0;
+          border: 0
+          height: 1px;
+          background-color: black;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
+        html {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+          font-size: 16px;
+        }
+
+        body {
+          padding: 3rem;
+          margin: 0;
         }
 
         * {
           box-sizing: border-box;
         }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p {
+          margin: 0;
+          padding: 0;
+          color: #111111;
+        }
+
+        a {
+          text-decoration: none;
+        }
       `}</style>
     </div>
-  )
+  );
 }
